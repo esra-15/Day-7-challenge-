148669641b4d8a6e3738687f94a28b6caf31b50c
Esra hamed <s2212176087@ku.edu.kw>
2025-08-21 18:18:31 +0300

    donr

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let name = "ESRA";
+let age = 20;
+
+const city = "kuwait";
+
+console.log(name);
+console.log(age);
+console.log(city);
+
+
+
+let name2 = prompt("what is your name ?");
+let age2 = prompt("what is your age ?");
+
+
+
+alert("HELLO " + name2 + " your age is " + age2 );
+
+console.log( "%c age name city" ,"color: green");
+
+
+
+
+
+
